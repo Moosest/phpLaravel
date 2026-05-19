dfe7c061e42a881a34d9274798f7f123188ec31d
Moosest <martenpoder@pm.me>
2026-05-19 20:42:53 +0300

    Functions and Filters

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
@@ -7,34 +7,51 @@
 </head>
 
 <body>
-    <h1>Recommended Books</h1>
-
     <?php
-    $books = [
-        [
-            'name' => 'Do Androids Dream of Electric Sheep',
-            'author' => 'Philip K. Dick',
-            'purchaseUrl' => 'http://example.com'
-        ],
-        [
-            'name' => 'Project Hail Mary',
-            'author' => 'Andy Weir',
-            'purchaseUrl' => 'http://example.com'
-        ]
-    ];
+        $books = [
+            [
+                'name' => 'Do Androids Dream of Electric Sheep',
+                'author' => 'Philip K. Dick',
+                'releaseYear' => 1968,
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'Project Hail Mary',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2021,
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'The Martian',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2011,
+                'purchaseUrl' => 'http://example.com'
+            ],
+        ];
+
+
+        function filterByAuthor($books, $author) {
+            $filteredBooks = [];
+
+            foreach ($books as $book) {
+                if ($book['author'] === $author) {
+                    $filteredBooks[] = $book;
+                }
+            }
+
+            return $filteredBooks;
+        }
     ?>
 
-
-    <p>
-        <?php foreach ($books as $book): ?>
+    <ul>
+        <?php foreach (filterByAuthor($books, 'Philip K. Dick') as $book): ?>
             <li>
                 <a href="<?= $book['purchaseUrl'] ?>">
-                    <?= $book['name']; ?>
+                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author']?>
                 </a>
             </li>
         <?php endforeach; ?>
-    </p>
-
+    </ul>
 </body>
 
 </html>

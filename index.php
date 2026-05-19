7ac016ea6f79069fe36b30d8757afe300e59fc72
Moosest <martenpoder@pm.me>
2026-05-19 21:22:03 +0300

    Lambda Functions

--- index.php
+++ index.php
@@ -8,46 +8,38 @@
 
 <body>
     <?php
-        $books = [
-            [
-                'name' => 'Do Androids Dream of Electric Sheep',
-                'author' => 'Philip K. Dick',
-                'releaseYear' => 1968,
-                'purchaseUrl' => 'http://example.com'
-            ],
-            [
-                'name' => 'Project Hail Mary',
-                'author' => 'Andy Weir',
-                'releaseYear' => 2021,
-                'purchaseUrl' => 'http://example.com'
-            ],
-            [
-                'name' => 'The Martian',
-                'author' => 'Andy Weir',
-                'releaseYear' => 2011,
-                'purchaseUrl' => 'http://example.com'
-            ],
-        ];
+    $books = [
+        [
+            'name' => 'Do Androids Dream of Electric Sheep',
+            'author' => 'Philip K. Dick',
+            'releaseYear' => 1968,
+            'purchaseUrl' => 'http://example.com'
+        ],
+        [
+            'name' => 'Project Hail Mary',
+            'author' => 'Andy Weir',
+            'releaseYear' => 2021,
+            'purchaseUrl' => 'http://example.com'
+        ],
+        [
+            'name' => 'The Martian',
+            'author' => 'Andy Weir',
+            'releaseYear' => 2011,
+            'purchaseUrl' => 'http://example.com'
+        ],
+    ];
+    $filteredBooks = array_filter($books, function ($book) {
+        return $book['author'] === 'Andy Weir';
+    });
 
 
-        function filterByAuthor($books, $author) {
-            $filteredBooks = [];
-
-            foreach ($books as $book) {
-                if ($book['author'] === $author) {
-                    $filteredBooks[] = $book;
-                }
-            }
-
-            return $filteredBooks;
-        }
     ?>
 
     <ul>
-        <?php foreach (filterByAuthor($books, 'Philip K. Dick') as $book): ?>
+        <?php foreach ($filteredBooks as $book): ?>
             <li>
                 <a href="<?= $book['purchaseUrl'] ?>">
-                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author']?>
+                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
                 </a>
             </li>
         <?php endforeach; ?>

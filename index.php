3f304088adc1def78770f62efab7203295f7b1ee
Moosest <martenpoder@pm.me>
2026-05-19 19:34:46 +0300

    Associative Arrays

--- index.php
+++ index.php
@@ -1,26 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
-<title>Demo</title>
+    <meta charset="UTF-8">
+    <title>Demo</title>
 </head>
+
 <body>
     <h1>Recommended Books</h1>
 
-    <?php 
-        $books = [
-        "Do Androids Dream of Electric Sheep",
-        "The Langoliers",
-        "Hail Mary"
-        ];
+    <?php
+    $books = [
+        [
+            'name' => 'Do Androids Dream of Electric Sheep',
+            'author' => 'Philip K. Dick',
+            'purchaseUrl' => 'http://example.com'
+        ],
+        [
+            'name' => 'Project Hail Mary',
+            'author' => 'Andy Weir',
+            'purchaseUrl' => 'http://example.com'
+        ]
+    ];
     ?>
 
 
-    <ul>
-        <?php foreach ($books as $book) : ?>
-            <li><?= $book ?></li>
+    <p>
+        <?php foreach ($books as $book): ?>
+            <li>
+                <a href="<?= $book['purchaseUrl'] ?>">
+                    <?= $book['name']; ?>
+                </a>
+            </li>
         <?php endforeach; ?>
-    </ul>
+    </p>
 
 </body>
+
 </html>
